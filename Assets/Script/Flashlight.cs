8b77dc200f38235f81939646c493cd9cd00139e3
IngusJr <13510101@std.stei.itb.ac.id>
2018-07-10 22:30:14 +0700

    And the god said, let be light and dark

---
+++ Assets/Script/Flashlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+    private bool isActive = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F))
+        {
+            isActive = !isActive;
+        }
+        GetComponent<Light>().enabled = isActive;
+	}
+}
